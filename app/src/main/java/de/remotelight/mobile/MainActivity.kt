478d04e23fa9134a2f8b38467b49254b82bb2b6a
Lars <drumberhd@gmail.com>
2020-10-21 17:55:57 +0200

    Added local dependencies

--- app/src/main/java/de/remotelight/mobile/MainActivity.kt
+++ app/src/main/java/de/remotelight/mobile/MainActivity.kt
@@ -2,10 +2,16 @@ package de.remotelight.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import de.lars.remotelightcore.RemoteLightCore
+import de.lars.remotelightcore.musicsync.sound.SoundProcessing
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val core: RemoteLightCore = RemoteLightCore(null, true)
     }
+
 }
